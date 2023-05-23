0849ff101d5ad54a0b19c57048ed8531b899f847
kanav-bhanushali <71547881+kanav-bhanushali@users.noreply.github.com>
2023-05-23 15:21:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 //https://teachablemachine.withgoogle.com/models/LF6Y8q54e/
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true});
+  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/LF6Y8q54e/model.json', modelReady);
+}
+
+function modelReady(){
+  classifier.classify(gotResults);
+}
